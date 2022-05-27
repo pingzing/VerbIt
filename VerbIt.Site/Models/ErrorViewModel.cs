fb04ec7e5047acab572c4da5703940c355b48773
pingzing <mcalistern@gmail.com>
2022-05-27 20:59:37 +0300

    Add project files.

---
+++ VerbIt.Site/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VerbIt.Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
