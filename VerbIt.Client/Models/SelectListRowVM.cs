cc3e5b62eb56b98cb33e2dba12f00ff9e5fbf8d3
pingzing <mcalistern@gmail.com>
2022-08-27 15:29:17 +0300

    More work on the Create Test page  - Can now select a master list  - Can now view the selected master list  - Skeleton of test creator in place

---
+++ VerbIt.Client/Models/SelectListRowVM.cs
@@ -0,0 +1,11 @@
+﻿namespace VerbIt.Client.Models
+{
+    public class SelectListRowVM
+    {
+        public Guid ListId { get; set; }
+        public Guid RowId { get; set; }
+        public int RowNum { get; set; }
+        public string[][] Words { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
--- VerbIt.Client/Pages/Dashboard/Tests/CreateTest.razor.cs
+++ VerbIt.Client/Pages/Dashboard/Tests/CreateTest.razor.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.WebUtilities;
+using System.Collections.Generic;
 using VerbIt.Client.Models;
+using VerbIt.Client.Services;
 using VerbIt.DataModels;
 
 namespace VerbIt.Client.Pages.Dashboard.Tests
@@ -13,16 +15,21 @@ namespace VerbIt.Client.Pages.Dashboard.Tests
         [Inject]
         private NavigationManager NavManager { get; set; } = null!;
 
+        [Inject]
+        private INetworkService _networkService { get; set; } = null!;
+
         private string TestNameFieldClass { get; set; } = "";
         private string TestName { get; set; } = "";
         private bool IsSaving { get; set; } = false;
 
-        private bool IsListPickerVisible { get; set; } = false;
+        private bool IsListSelectorVisible { get; set; } = false;
         private Guid? ChosenMasterList { get; set; } = null;
-        private List<SavedMasterList>? SavedMasterLists = null;
-        private List<CreateTestRowVM> RowList { get; set; } = new List<CreateTestRowVM>();
+        private string? ChosenMasterListName { get; set; } = null;
+        private List<SelectListRowVM>? ChosenMasterListRows { get; set; } = null;
+        private List<SavedMasterList>? SavedMasterLists { get; set; } = null;
+        private List<CreateTestRowVM> TestRows { get; set; } = new List<CreateTestRowVM>();
 
-        protected override Task OnInitializedAsync()
+        protected override async Task OnInitializedAsync()
         {
             Layout.Title = "Tests - Create";
             Layout.BackButtonText = "↑ Go up to Tests";
@@ -41,23 +48,62 @@ namespace VerbIt.Client.Pages.Dashboard.Tests
             // don't show the picker, but try to fetch it.
             if (ChosenMasterList != null)
             {
-                IsListPickerVisible = false;
+                IsListSelectorVisible = false;
 
                 // Somewhere after this (inline? in a method?) try to fetch the list.
                 // If it exists and we get it back display it.
+                await FetchAndDisplayMasterList(ChosenMasterList.Value, CancellationToken.None);
             }
             else
             {
                 // If we DON'T, show the masterListSelector, and prompt the user to pick one.
-                IsListPickerVisible = true;
-            }
+                IsListSelectorVisible = true;
+                var savedMasterLists = await _networkService.GetMasterLists(CancellationToken.None);
+                if (savedMasterLists == null)
+                {
+                    // TODO: Display sadness
+                    return;
+                }
 
-            return Task.CompletedTask;
+                SavedMasterLists = savedMasterLists.ToList();
+            }
         }
 
-        internal void MasterListClicked(SavedMasterList clickedList) { }
+        internal async void MasterListClicked(SavedMasterList clickedList)
+        {
+            await FetchAndDisplayMasterList(clickedList.ListId, CancellationToken.None);
+        }
 
-        internal void AddRowClicked() { }
+        internal async Task FetchAndDisplayMasterList(Guid listId, CancellationToken token)
+        {
+            var fetchedList = await _networkService.GetMasterList(listId, CancellationToken.None);
+            if (fetchedList == null)
+            {
+                // TODO: Show error, and be sad
+                return;
+            }
+
+            ChosenMasterList = listId;
+            ChosenMasterListName = fetchedList[0].ListName;
+            ChosenMasterListRows = fetchedList
+                .Select(
+                    x =>
+                        new SelectListRowVM
+                        {
+                            ListId = x.ListId,
+                            RowId = x.RowId,
+                            RowNum = x.RowNum,
+                            Words = x.Words,
+                            IsSelected = false
+                        }
+                )
+                .ToList();
+
+            // Changing the property only triggers a re-render the second time for some reason.
+            // So, force it with a manual StateHasChanged().
+            IsListSelectorVisible = false;
+            StateHasChanged();
+        }
 
         internal void SaveListClicked() { }
 
